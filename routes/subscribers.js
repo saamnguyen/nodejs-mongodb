ef5282c1e9c42ed9befbe17a4b56808d4f957897
finn-01 <nguyensam576@gmail.com>
2022-04-18 14:59:36 +0700

    getbyid

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,19 +1,52 @@
 const express = require("express");
 const router = express.Router();
+const Subscriber = require("../model/subscriber");
 
 //Get all
-router.get("/", (req, res) => {
-	res.send("Hello GET");
+router.get("/", async (req, res) => {
+	try {
+		const subscribers = await Subscriber.find();
+		res.json(subscribers);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
 });
 
-router.get("/:id", (req, res) => {
-	res.send(req.params.id);
+router.get("/:id", getSubcriber, (req, res) => {
+	// res.send(res.subscriber.name);
+	res.json(res.subscriber);
 });
 
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+	const subscriber = new Subscriber({
+		name: req.body.name,
+		subscribedToChannel: req.body.subscribedToChannel,
+	});
+	try {
+		const newSubscriber = await subscriber.save();
+		res.status(201).json(newSubscriber);
+	} catch (error) {
+		res.status(400).json({ message: error.message });
+	}
+});
 
 router.patch("/:id", (req, res) => {});
 
 router.delete("/:id", (req, res) => {});
+
+async function getSubcriber(req, res, next) {
+	let subscriber;
+	try {
+		subscriber = await Subscriber.findById(req.params.id);
+		if (subscriber === null) {
+			return res.status(404).json({ message: "Canot find subscriber" });
+		}
+	} catch (error) {
+		return res.status(404).json({ message: error.message });
+	}
+
+	res.subscriber = subscriber;
+	next();
+}
 
 module.exports = router;
